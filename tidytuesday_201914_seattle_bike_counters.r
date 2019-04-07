b6429feabb76b80c3f62385aa8e77af97a454d1a
Torsten Sprenger <mail@spren9er.de>
2019-04-07 13:03:46 +0200

    Fix code indentation in 14|2019

--- tidytuesday_201914_seattle_bike_counters.r
+++ tidytuesday_201914_seattle_bike_counters.r
@@ -14,7 +14,7 @@ raw_data <- readr::read_csv(
 )
 
 bike_traffic <- raw_data %>%
-  filter( !is.na(bike_count), bike_count < 2000, date >= ymd(20140101))
+  filter(!is.na(bike_count), bike_count < 2000, date >= ymd(20140101))
 
 bike_traffic_totals <- bike_traffic %>%
   mutate(wday = wday(date, label = TRUE), hour = hour(date)) %>%
