7880e171484797858f18022634f55f70325debbc
Torsten Sprenger <mail@spren9er.de>
2019-11-18 22:24:59 +0100

    Tidy code of tidytuesday 46|2019 contribution and add more comments

--- tidytuesday_201946_cran_packages.r
+++ tidytuesday_201946_cran_packages.r
@@ -10,29 +10,16 @@ path <-
 
 data <- read_csv(paste0(path, 'loc_cran_packages.csv'))
 
-# popular programming languages from Tiobe Index (Nov. 2019)
+# most popular programming languages from TIOBE Index (Nov. 2019) found in data
+# (only languages with position <= 16 are considered)
 popular_languages <- c(
   'Java', 'C', 'Python', 'C++', 'C#', 'Visual Basic', 'JavaScript', 'PHP', 'SQL', 'Ruby', 'Objective C++', 'Assembly', 'R'
 )
 
+# number of packages to display
 number_of_pkgs <- 300
 
-top_language_colors <- list(
-  'Assembly'   = '#efb306',
-  'C'          = '#eb990c',
-  'C++'        = '#e8351e',
-  'JavaScript' = '#852f88',
-  'Java'       = '#cd023d',
-  'R'          = '#7db954',
-  'Python'     = '#0f8096',
-  'Ruby'       = '#4e54ac',
-  'SQL'        = '#17a769',
-  'All'        = '#000000'
-)
-
-colors <- as.vector(unlist(top_language_colors))
-levels <- names(top_language_colors)
-
+# find largest packages written in popular languages
 top_packages <- data %>%
   filter(language %in% popular_languages) %>%
   group_by(pkg_name) %>%
@@ -41,109 +28,133 @@ top_packages <- data %>%
   head(number_of_pkgs) %>%
   select(pkg_name, total_code)
 
+# all popular languages per package
 top_languages_per_pkg <- data %>%
   filter(
     pkg_name %in% top_packages$pkg_name,
     language %in% popular_languages
   ) %>%
-  left_join(top_packages, by = 'pkg_name') %>%
-  mutate(language = factor(language, levels = levels)) %>%
-  arrange(language, pkg_name)
+  arrange(pkg_name, desc(code)) %>%
+  group_by(pkg_name) %>%
+  mutate(
+    main = row_number() == 1, # main language of package should be opaque
+    total_code = sum(code)
+  ) %>%
+  ungroup() %>%
+  select(language, pkg_name, code, total_code, main)
 
-top_languages <- top_languages_per_pkg %>%
-  group_by(language) %>%
-  summarize(total_code = sum(code)) %>%
-  arrange(language) %>%
-  ungroup()
+# only following languages found in given packages
+(top_languages <- top_languages_per_pkg %>%
+  pull(language) %>%
+  unique %>%
+  sort)
+
+top_language_colors <- c(
+  '#efb306',
+  '#eb990c',
+  '#e8351e',
+  '#cd023d',
+  '#852f88',
+  '#4e54ac',
+  '#0f8096',
+  '#7db954',
+  '#17a769',
+  '#000000'
+)
+
+names(top_language_colors) <- c(
+  'Assembly',
+  'C',
+  'C++',
+  'JavaScript',
+  'Java',
+  'R',
+  'Python',
+  'Ruby',
+  'SQL',
+  'All'
+)
 
 edges1 <- top_languages_per_pkg %>%
-  transmute(from = language, to = pkg_name, total_code = code)
+  transmute(from = language, to = pkg_name, total_code = code, main)
 
-edges2 <- top_languages %>%
+edges2 <- top_languages_per_pkg %>%
+  count(language, wt = code, name = 'total_code') %>%
   transmute(
-    from = 'All',
+    from = '',
     to = language,
     total_code,
-    alpha = 1
+    main = TRUE
   )
-
-vertices1 <- edges1 %>%
-  group_by(to) %>%
-  top_n(1, total_code) %>%
-  ungroup() %>%
-  transmute(to, label = to, code = total_code, color = from) %>%
-  left_join(
-    count(edges1, to, wt = total_code, name = 'total_code'), by = 'to'
-  ) %>%
-  rename(node = to) %>%
-  mutate(level = 1, color = factor(color, levels = levels)) %>%
-  arrange(color, node)
-
-edges1 <- left_join(
-  edges1,
-  edges1 %>%
-    left_join(vertices1, by = c('to' = 'node', 'from' = 'color')) %>%
-    transmute(from, to, alpha = level) %>%
-    replace_na(list(alpha = 0))
-)
 
 edges <- bind_rows(edges1, edges2)
 
+top_language_per_pkg <- top_languages_per_pkg %>%
+  filter(main)
+
+vertices1 <- top_language_per_pkg %>%
+  transmute(
+    node = pkg_name, language, total_code, level = 1
+  )
+
 vertices2 <- edges2 %>%
   transmute(
-    node = to, label = to, code = total_code, color = to, total_code, level = 2
-  ) %>%
-  arrange(node)
+    node = to, language = to, total_code, level = 2
+  )
 
 vertices3 = tibble(
-  node = 'All', label = '', code = 0, color = NA, total_code = 0, level = 3
+  node = '', language = NA, total_code = 0, level = 3
 )
 
 vertices = bind_rows(vertices1, vertices2, vertices3) %>%
-  mutate(radius = total_code**(1.8))
+  mutate(
+    radius = total_code**(1.8), # scaling circles
+    language = factor(language, names(top_language_colors))
+  ) %>%
+  arrange(level, language, node)
 
 graph <- graph_from_data_frame(edges, vertices = vertices)
 
-# create custom layout by updating circle layout
+# create custom layout by updating existing circle layout
 layout <- create_layout(graph, layout = 'circle')
 
-layout1 <- layout %>%
+outer_circle <- layout %>%
   filter(level == 1) %>%
-  mutate(color = factor(color, levels = levels)) %>%
-  arrange(color, desc(label)) %>%
+  mutate(language = factor(language, names(top_language_colors))) %>%
+  arrange(language, desc(name)) %>%
   mutate(
     x = cos((row_number() - 1) / number_of_pkgs * 2 * pi),
     y = sin((row_number() - 1) / number_of_pkgs * 2 * pi)
   )
 
+# positioning circle centers manually by specifying polar coords
 angles <- c(3, 43, 119, 160, 178, 255, 350, 190, 340, 0)
 radii <- c(0.8, 0.5, 0.6, 0.4, 0.65, 0.45, 0.6, 0.7, 0.38, 0)
 centers <- tibble(
-  x = radii * map_dbl(angles, ~ cos(. / 180 * pi)),
-  y = radii * map_dbl(angles, ~ sin(. / 180 * pi))
+  x = radii * cos(angles / 180 * pi),
+  y = radii * sin(angles / 180 * pi)
 )
+inner_circle <- bind_cols(centers, select(filter(layout, level != 1), -x, -y))
 
-layout2 <- bind_cols(centers, select(filter(layout, level != 1), -x, -y))
-
-layout[] <- bind_rows(layout1, layout2) %>%
+layout[] <- bind_rows(outer_circle, inner_circle) %>%
   arrange(ggraph.index)
 
-ggraph(layout, circular = TRUE) +
+ggraph(layout) +
   geom_edge_diagonal(
-    aes(edge_color = node1.color, edge_alpha = as.factor(alpha)),
+    aes(edge_color = node1.language, edge_alpha = as.factor(main)),
     edge_width = 0.3, show.legend = FALSE
   ) +
   geom_node_point(
-    aes(size = radius, color = color),
+    aes(size = radius, color = language),
     alpha = 0.6, show.legend = FALSE
   ) +
   geom_node_text(
     aes(
       x = 1.0175 * x,
       y = 1.0175 * y,
-      label = label,
+      label = name,
       angle = -((-node_angle(x, y) + 90) %% 180) + 90,
-      filter = !(label %in% top_languages$language)
+      filter = !(name %in% top_languages)
     ),
     size = 2, hjust = 'outward', family = 'Oswald'
   ) +
@@ -151,11 +162,10 @@ ggraph(layout, circular = TRUE) +
     aes(
       x = x,
       y = y,
-      label = label,
-      filter = label %in% top_languages$language
+      label = name,
+      filter = name %in% top_languages
     ),
-    size = 6, hjust = 0.5, family = 'Oswald',
-    point.padding = NA, repel = TRUE
+    size = 6, hjust = 0.5, family = 'Oswald'
   ) +
   geom_node_text(
     aes(
@@ -166,19 +176,18 @@ ggraph(layout, circular = TRUE) +
         format(total_code, big.mark = ','),
         total_code
       ),
-      filter = label %in% top_languages$language
+      filter = name %in% top_languages
     ),
-    size = 3, hjust = 0.5, family = 'Oswald',
-    point.padding = NA, repel = TRUE
+    size = 3, hjust = 0.5, family = 'Oswald'
   ) +
-  scale_edge_color_manual(values = colors, guide = FALSE) +
-  scale_color_manual(values = colors, guide = FALSE) +
-  scale_size_area(max_size = 150, guide = FALSE) +
-  scale_edge_alpha_manual(values = c(0.15, 1), guide = FALSE) +
+  scale_edge_color_manual(values = top_language_colors) +
+  scale_color_manual(values = top_language_colors) +
+  scale_size_area(max_size = 150) +
+  scale_edge_alpha_manual(values = c(0.15, 1)) +
   coord_fixed() +
   labs(
     title = 'LOC of Popular Programming Languages in 300 CRAN Packages',
-    subtitle = 'considered are largest CRAN packages written in one (or more) of top 16 programming languages from Tiobe Index (Nov. 2019)',
+    subtitle = 'considered are largest CRAN packages written in one (or more) of top 16 programming languages from TIOBE Index (Nov. 2019)',
     caption = '#tidytuesday 46|2019 spren9er'
   ) +
   theme_void() +
